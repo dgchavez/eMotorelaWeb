fe555858611d99ff2bb0e53afdf5aa0a1419874b
Dave Edward Chavez <cdaveedward@gmail.com>
2025-06-10 21:18:44 +0800

    updates

--- resources/views/admin/operators/show-modal.blade.php
+++ resources/views/admin/operators/show-modal.blade.php
@@ -127,11 +127,6 @@
             </a>
         @endif
 
-        <!-- Close -->
-        <button type="button" 
-                @click="show = false"
-                class="inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Close
-        </button>
+    
     </div>
 </div> 
--- resources/views/admin/operatorsIndex.blade.php
+++ resources/views/admin/operatorsIndex.blade.php
@@ -227,33 +227,7 @@
         </div>
     </div>
 
-    <!-- Operator Details Modal -->
-    <div id="operatorDetailsModal" 
-         x-data="{ show: false }" 
-         x-show="show" 
-         @operator-modal.window="show = true" 
-         @close-operator-modal.window="show = false"
-         class="fixed inset-0 z-50 overflow-y-auto" 
-         style="display: none;">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <!-- Background overlay -->
-            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" @click="show = false"></div>
 
-            <!-- Modal panel -->
-            <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-4xl sm:w-full">
-                <div id="operatorDetailsContent">
-                    <!-- Content will be loaded here -->
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="button" 
-                            @click="show = false"
-                            class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
-                        Close
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
 
     @push('head-scripts')
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
